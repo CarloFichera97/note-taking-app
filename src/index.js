7c4a3b568558be59f333fdeb6d3b90c5dbbf7d9e
Carlo Fichera <Carlo.fichera97@gmail.com>
2021-06-01 16:22:39 +0100

    Added functionality, Added local storage fetching and storing functionalities, added stateless functional components

--- src/index.js
+++ src/index.js
@@ -2,15 +2,11 @@ require("file-loader?name=[name].[ext]!./index.html");
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Implement:
-//- Capitalize Note Functionality
-//- Note Time
-//- Total Characters Inserted
-
 class NoteTakingApp extends React.Component {
   constructor(props) {
     super(props);
     this.handleDeleteNotes = this.handleDeleteNotes.bind(this);
+    this.handleDeleteNote = this.handleDeleteNote.bind(this);
     this.handleAddNote = this.handleAddNote.bind(this);
     this.capitalizeInput = this.capitalizeInput.bind(this);
     this.state = {
@@ -20,18 +16,76 @@ class NoteTakingApp extends React.Component {
     };
   }
 
+  //Stores variables in local storage everytime there is a component update
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.notes.length !== this.state.notes.length) {
+      const jsonNotes = JSON.stringify(this.state.notes);
+      const jsonNotesTime = JSON.stringify(this.state.notesTime);
+      const jsonNotesLength = JSON.stringify(this.state.notesLength);
+      localStorage.setItem("notes", jsonNotes);
+      localStorage.setItem("notes_time", jsonNotesTime);
+      localStorage.setItem("notes_length", jsonNotesLength);
+      //console.log(`Notes: ${jsonNotes}`);
+      //console.log(`Notes -- Time: ${jsonNotesTime}`);
+      //console.log(`Notes -- Lenght: ${jsonNotesLength}`);
+    }
+  }
+
+  //Fetches in local storage values to display on screen first time the page gets mounted
+  componentDidMount(prevProps, prevState) {
+    try {
+      const jsonNotes = localStorage.getItem("notes");
+      const jsonNotesTime = localStorage.getItem("notes_time");
+      const jsonNotesLength = localStorage.getItem("notes_length");
+
+      const notes = JSON.parse(jsonNotes);
+      const notesLength = JSON.parse(jsonNotesLength);
+      const notesTime = JSON.parse(jsonNotesTime);
+
+      if (notes) {
+        this.setState(() => ({
+          notes: notes,
+          notesLength: notesLength,
+          notesTime: notesTime,
+        }));
+      }
+    } catch (e) {}
+  }
+
+  //Capitalize the function input
   capitalizeInput(input) {
     return input[0].toUpperCase() + input.slice(1);
   }
 
+  //Deletes all notes
   handleDeleteNotes() {
     this.setState(() => {
       return {
         notes: [],
+        notesLength: [],
+        notesTime: [],
       };
     });
   }
 
+  //Delete Single Note
+  handleDeleteNote(noteToRemove, timeToRemove, charactersToRemove) {
+    this.setState((prevState) => {
+      return {
+        notes: prevState.notes.filter((note) => {
+          return noteToRemove !== note;
+        }),
+        notesLength: prevState.notesLength.filter((note) => {
+          return charactersToRemove !== note;
+        }),
+        notesTime: prevState.notesTime.filter((note) => {
+          return timeToRemove !== note;
+        }),
+      };
+    });
+  }
+
+  // Add a note with time and number of characters to screen
   handleAddNote(note) {
     if (!note) {
       return "Enter Valid Value to Add Item";
@@ -41,7 +95,6 @@ class NoteTakingApp extends React.Component {
 
     this.setState((prevState) => {
       let time = [new Date().toUTCString()];
-      console.log(time);
       return {
         notes: prevState.notes.concat([this.capitalizeInput(note)]),
         notesLength: prevState.notesLength.concat([
@@ -65,6 +118,7 @@ class NoteTakingApp extends React.Component {
           notesLength={this.state.notesLength}
           notesTime={this.state.notesTime}
           handleDeleteNotes={this.handleDeleteNotes}
+          handleDeleteNote={this.handleDeleteNote}
         />
         <AddNotes
           handleAddNote={this.handleAddNote}
@@ -75,41 +129,47 @@ class NoteTakingApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  //Method to define this react componen
-  render() {
-    return (
-      <div>
-        <h1> {this.props.title} </h1>
-        <h2>{this.props.subtitle}</h2>
-      </div>
-    );
-  }
-}
+const Header = (props) => {
+  return (
+    <div>
+      <h1> {props.title} </h1>
+      <h2>{props.subtitle}</h2>
+    </div>
+  );
+};
 
-class Notes extends React.Component {
-  render() {
-    return (
-      <div>
-        <button onClick={this.props.handleDeleteNotes}>Remove All</button>
-        {this.props.notes.map((i, note) => (
-          <div >
-            <strong>Note{note + 1}:</strong> {this.props.notes[note]}
-            <li key={`Date--${i + 1}`}>
-              <strong >Date: </strong> {this.props.notesTime[note]}
-              {}
-            </li>
-            <li key={`Note--${i + 1}`}>
-              <strong>Number Of Characters: </strong>
-              {this.props.notesLength[note]}
-              {}
-            </li>
-          </div>
-        ))}
-      </div>
-    );
-  }
-}
+const Notes = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteNotes}>Remove All</button>
+      {props.notes.map((i, note) => (
+        <div>
+          <strong>Note{note + 1}:</strong> {props.notes[note]}
+          <button
+            onClick={(e) =>
+              props.handleDeleteNote(
+                props.notes[note],
+                props.notesTime[note],
+                props.notesLength[note]
+              )
+            }
+          >
+            Remove note
+          </button>
+          <li key={`Date--${i + 1}`}>
+            <strong>Date: </strong> {props.notesTime[note]}
+            {}
+          </li>
+          <li key={`Note--${i + 1}`}>
+            <strong>Number Of Characters: </strong>
+            {props.notesLength[note]}
+            {}
+          </li>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 class AddNotes extends React.Component {
   constructor(props) {
@@ -123,7 +183,6 @@ class AddNotes extends React.Component {
     e.preventDefault();
     let note = document.querySelector(".note").value.trim();
     const error = this.props.handleAddNote(note);
-    console.log(error);
     document.querySelector(".note").value = "";
 
     this.setState(() => {
